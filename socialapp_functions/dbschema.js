47f393dda64ce94aa4388e7a2032d6f2c9e49a2a
Alfredo <alfredodomin91@gmail.com>
2020-03-24 22:27:27 -0500

    updated dbschema file for reference, will work on add user profile info routes

--- socialapp_functions/dbschema.js
+++ socialapp_functions/dbschema.js
@@ -1,6 +1,17 @@
 // reference file
 
 let db = {
+    users:[
+        {
+            userId: 'dh23gg5h67g578j5gf89',
+            email: 'alfredo@email.com',
+            createdAt: '2020-03-21T17:10:50.205Z',
+            imageUrl: 'image/alksjflksdfj/laksjfldskf',
+            bio: 'Hello, I am a retired chemist',
+            website: 'https://user.com',
+            location: 'Austin, Tx'
+        }
+    ],
     screams: [
         {
             userHandle: 'userHandle',
