827a2cc409e5b9c9402489cf8749eaf24349b492
Alfredo <alfredodomin91@gmail.com>
2020-04-01 12:12:58 -0500

    included context api and style different components

--- socialapp_functions/functions/src/context.js
+++ socialapp_functions/functions/src/context.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import {storeProducts, detailProduct} from './data'
 
 const ProductContext = React.createContext();
 //Provider
 //Consumer
 
-function ProductProvider(props) {
+class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detailProduct
+    }
+
+    handleDetail= () =>{
+        console.log('hello from detail')
+    }
+    addToCart= () =>{
+        console.log('hello from add to cart')
+    }
+render(){
   return (
-    <ProductContext.Provider value='hello from context'>
-        {props.children}
+    <ProductContext.Provider value={{
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart:this.addToCart
+    }}>
+        {this.props.children}
     </ProductContext.Provider>
 
   );
+}
 }
 
 const ProductConsumer = ProductContext.Consumer;
